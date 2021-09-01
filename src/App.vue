ab5a230edf9d7949ca1c0736fb74cbcdaf71a45d
陈杰 <5353543+lvyier@user.noreply.gitee.com>
2021-09-01 17:58:30 +0800

    部署文件前期需要的插件

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
     <div>Hello</div>
+    <div>
+      <i class="iconfont iconwenda"></i>
+      <van-button type="primary" class="box1">主要按钮</van-button>
+      <div class="box"></div>
+    </div>
     <router-view/>
   </div>
 </template>
@@ -10,4 +15,12 @@ export default {
 }
 </script>
 <style lang="less">
+.box{
+  width: 350px;
+  height: 64px;
+  background-color: pink;
+}
+.box1{
+  width: 375px!important;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -4,7 +4,13 @@ import router from './router'
 import store from './store'
 // 全局样式文件
 import './styles/index.less'
+// 动态加载设置REM基准值
+import 'amfe-flexible'
+// 引入vant相关文件
+import Vant from 'vant'
+import 'vant/lib/index.css'
 
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
